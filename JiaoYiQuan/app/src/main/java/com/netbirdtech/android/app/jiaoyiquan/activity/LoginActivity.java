40efb3e9f2e60c0551aad5823d7e29e4d529b6e3
wzh <445782524@qq.com>
2016-02-29 13:43:45 +0800

    切图和接口文档

---
+++ JiaoYiQuan/app/src/main/java/com/netbirdtech/android/app/jiaoyiquan/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.netbirdtech.android.app.jiaoyiquan.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.netbirdtech.android.app.jiaoyiquan.R;
+
+/**
+ * 登录注册页面
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
